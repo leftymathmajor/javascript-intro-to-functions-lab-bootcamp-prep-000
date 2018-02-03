cdd416ba49615b4740a8b1197d8d5b332e596af1
Kristine Wise <leftymathmajor@yahoo.com>
2018-02-03 04:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string) {return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
-function logShout(string){console.log(string.to)
+function logShout(string){console.log(string.toLowerCase())}
